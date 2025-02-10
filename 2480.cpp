776eea3c2c837d078c494e57de722a8f4e1ec7ce
Ruka <ruka@ruka.my>
2025-02-11 02:11:11 +0900

    2025.2.11 조건문

---
+++ 2480.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int a, b, c, prize;
+
+    if(a == b && b == c) prize = 10000 + a * 1000;
+    else if(a == b )
+}
